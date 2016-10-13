9b7ab958b6117d9241a7b22847f6223f17d09775
Sergio <smartinezchueca@gmail.com>
2016-10-13 12:19:04 +0200

    SplashActivity

--- app/src/main/java/org/zterr/frontend/android/SplashActivity.java
+++ app/src/main/java/org/zterr/frontend/android/SplashActivity.java
@@ -2,12 +2,34 @@ package org.zterr.frontend.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
+    TextView nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_splash);
+
+
+        getSupportActionBar().hide();
+
+        Animation transpare;
+        transpare= AnimationUtils.loadAnimation(this,R.anim.transparentar);
+       // transpare.reset();
+        logo.startAnimation(transpare);
     }
 }
